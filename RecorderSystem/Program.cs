40ec27cfa7d76f19e7d347fbc59fe6b18bbae8a4
Andrius Statkevicius <andrius.statkevicius@gmail.com>
2022-01-24 08:33:51 +0200

    First commit

---
+++ RecorderSystem/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+//builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(1028));
+builder.WebHost.UseUrls("http://localhost:9874");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
